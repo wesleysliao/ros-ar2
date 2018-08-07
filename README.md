b8e481a84c96d0857bab5e670796c9d53e86bd1d
wesliao <wesliao@iu.edu>
2018-08-07 15:24:39 -0400

    Added files following slaterobots ros_control tutorial. Haven't tried compiling, needs ar2cpp package to be created

---
+++ README.md
@@ -0,0 +1,5 @@
+
+ros_control interface for Chris Annin's AR2 Robot
+
+##TODO
+-Update joint angle, velocity, and acceleration limits in config/joint_limits.yaml
---
+++ src/ar2_hw_interface_node.cpp
@@ -0,0 +1,11 @@
+#include <ar2_hw_interface/ar2_hardware_interface.h>
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "ar2_hardware_interface");
+    ros::NodeHandle nh;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+    ar2_hardware_interface::AR2HardwareInterface AR2(nh);
+    ros::spin();
+    return 0;
+}
